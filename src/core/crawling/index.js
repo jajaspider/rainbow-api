46e8196e0403f6cdd8c2e743b4eabc8fea3dc314
jajaspider <jaja_spider@naver.com>
2024-01-03 01:30:19 +0900

    add shinhan parsing by node-schedule

--- src/core/crawling/index.js
+++ src/core/crawling/index.js
@@ -1,25 +1,25 @@
-const maplestory = require('./maplestory');
+require("./shinhan");
+
+const maplestory = require("./maplestory");
 const lostark = require("./lostark");
 
 class Crawling {
-    constructor() {
+  constructor() {}
+  async init() {
+    await maplestory.init();
+    await lostark.init();
 
-    }
-    async init() {
-        await maplestory.init();
-        await lostark.init();
+    this.maplestoryNotice = setInterval(async () => {
+      await maplestory.crawlingNotice();
+    }, 5000);
+    this.maplestoryTestNotice = setInterval(async () => {
+      await maplestory.crawlingTestNotice();
+    }, 5000);
 
-        this.maplestoryNotice = setInterval(async () => {
-            await maplestory.crawlingNotice();
-        }, 5000);
-        this.maplestoryTestNotice = setInterval(async () => {
-            await maplestory.crawlingTestNotice();
-        }, 5000);
-
-        this.lostarkNotice = setInterval(async () => {
-            await lostark.crawlingNotice();
-        }, 5000);
-    }
+    this.lostarkNotice = setInterval(async () => {
+      await lostark.crawlingNotice();
+    }, 5000);
+  }
 }
 
 const crawling = new Crawling();
