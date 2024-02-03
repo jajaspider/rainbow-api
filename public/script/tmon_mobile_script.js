c089ddcf80095b2e3f72cd3b0078a7de4aee1ce7
jajaspider <jaja_spider@naver.com>
2024-02-03 16:18:50 +0900

    chores

--- public/script/tmon_mobile_script.js
+++ public/script/tmon_mobile_script.js
@@ -1,7 +1,7 @@
 function run() {
   var script = document.createElement("script");
   script.async = 1;
-  script.src = "https://5999.kr/api/script/tmon_payment.js";
+  script.src = "https://5999.kr/api/script/tmon_payment_mobile.js";
   script.charset = "UTF-8";
   document.getElementsByTagName("body")[0].appendChild(script);
 }
