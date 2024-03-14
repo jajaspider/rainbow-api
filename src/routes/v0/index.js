9fd8fdfdda676c032ffe2ac8e0574dc2983dedaf
SonaAPI <jaja_spider@naver.com>
2024-03-15 00:06:25 +0900

    feat(#90): add ncnc router, service
    router
    get category, brand, itemByName
    
    service
    get category, brand, item, itemStatus

--- src/routes/v0/index.js
+++ src/routes/v0/index.js
@@ -6,11 +6,13 @@ const lostarkRouter = require("./lostark");
 const maplestoryRouter = require("./maplestory");
 const imageRouter = require("./images");
 const theMoreRouter = require("./themore/index");
+const ncncRouter = require("./ncnc");
 
 router.use("/selection", selectionRouter);
 router.use("/lostark", lostarkRouter);
 router.use("/maplestory", maplestoryRouter);
 router.use("/images", imageRouter);
 router.use("/themore", theMoreRouter);
+router.use("/ncnc", ncncRouter);
 
 module.exports = router;
