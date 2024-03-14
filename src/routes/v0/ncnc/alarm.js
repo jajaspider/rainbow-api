1bb119450c6698db87b90bd27e6e6d68d674e148
SonaAPI <jaja_spider@naver.com>
2024-03-15 01:04:56 +0900

    feat(#90): add chat_id in alarm router

--- src/routes/v0/ncnc/alarm.js
+++ src/routes/v0/ncnc/alarm.js
@@ -4,7 +4,7 @@ const _ = require("lodash");
 const DB = require("../../../models"),
   Alarm = DB.NCNCAlarm;
 
-router.get("/get", async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     let alram = await Alarm.find({});
     return res.json(alram);
@@ -36,17 +36,31 @@ router.put("/enable", async (req, res, next) => {
       });
     }
 
+    let chatId = _.get(req.query, "chatId");
+    if (_.isEmpty(brandId)) {
+      throw new RainbowError({
+        httpCode: 400,
+        error: ERROR_CODE.MISSING_PARAMETER,
+        reason: `require chatId parameter`,
+      });
+    }
+
     let alarmObj = {
       brandId,
       id: itemId,
       active: true,
+      chat_id: chatId,
     };
-    await Alarm.findOneAndUpdate({ brandId: brandId, id: itemId }, alarmObj, {
-      upsert: true,
-      new: true,
-    });
+    await Alarm.findOneAndUpdate(
+      { brandId: brandId, id: itemId, chat_id: chatId },
+      alarmObj,
+      {
+        upsert: true,
+        new: true,
+      }
+    );
 
-    return res.status(204);
+    return res.json();
   } catch (e) {
     if (e instanceof RainbowError) {
       return res.status(e.httpCode).send(`${e.error.message} : ${e.reason}`);
@@ -75,17 +89,31 @@ router.put("/disable", async (req, res, next) => {
       });
     }
 
+    let chatId = _.get(req.query, "chatId");
+    if (_.isEmpty(brandId)) {
+      throw new RainbowError({
+        httpCode: 400,
+        error: ERROR_CODE.MISSING_PARAMETER,
+        reason: `require chatId parameter`,
+      });
+    }
+
     let alarmObj = {
       brandId,
       id: itemId,
       active: false,
+      chat_id: chatId,
     };
-    await Alarm.findOneAndUpdate({ brandId: brandId, id: itemId }, alarmObj, {
-      upsert: true,
-      new: true,
-    });
+    await Alarm.findOneAndUpdate(
+      { brandId: brandId, id: itemId, chat_id: chatId },
+      alarmObj,
+      {
+        upsert: true,
+        new: true,
+      }
+    );
 
-    return res.status(204);
+    return res.json();
   } catch (e) {
     if (e instanceof RainbowError) {
       return res.status(e.httpCode).send(`${e.error.message} : ${e.reason}`);
